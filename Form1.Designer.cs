7283f27f9d9a8e89f2462547ecba7a5bc84cd382
Beyza <112386181+Kirtan51@users.noreply.github.com>
2023-03-13 16:14:12 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace hesapmakinesi1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ekranlabel1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ekranlabel1
+            // 
+            this.ekranlabel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            resources.ApplyResources(this.ekranlabel1, "ekranlabel1");
+            this.ekranlabel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ekranlabel1.Name = "ekranlabel1";
+            this.ekranlabel1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightPink;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightPink;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightPink;
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightPink;
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LightPink;
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.LightPink;
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.Name = "button6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.LightPink;
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.Name = "button7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.LightPink;
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.Name = "button8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.LightPink;
+            resources.ApplyResources(this.button9, "button9");
+            this.button9.Name = "button9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Red;
+            resources.ApplyResources(this.button10, "button10");
+            this.button10.Name = "button10";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.LightPink;
+            resources.ApplyResources(this.button11, "button11");
+            this.button11.Name = "button11";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.SystemColors.ControlDark;
+            resources.ApplyResources(this.button12, "button12");
+            this.button12.Name = "button12";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            resources.ApplyResources(this.button13, "button13");
+            this.button13.Name = "button13";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            resources.ApplyResources(this.button14, "button14");
+            this.button14.Name = "button14";
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            resources.ApplyResources(this.button15, "button15");
+            this.button15.Name = "button15";
+            this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.BackColor = System.Drawing.Color.LemonChiffon;
+            resources.ApplyResources(this.button16, "button16");
+            this.button16.Name = "button16";
+            this.button16.UseVisualStyleBackColor = false;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ekranlabel1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.TransparencyKey = System.Drawing.Color.SeaGreen;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ekranlabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hesapmakinesi1
+{
+    public partial class Form1 : Form
+    {
+
+        char _islem;        //her yerde geçerli olduğu anlaşılsın diye çizgi koydum
+        bool _ekrantemizmi;
+        int _ilksayi;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_ekrantemizmi) ekranlabel1.Text = "";
+            _ekrantemizmi= false;
+            if (ekranlabel1.Text == "0") ekranlabel1.Text = " ";
+            ekranlabel1.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_ekrantemizmi) ekranlabel1.Text = "";
+            _ekrantemizmi = false;
+            if (ekranlabel1.Text == "0") ekranlabel1.Text = "";
+            ekranlabel1.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (_ekrantemizmi) ekranlabel1.Text = "";
+            _ekrantemizmi = false;
+            if (ekranlabel1.Text == "0") ekranlabel1.Text = "";
+            ekranlabel1.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (_ekrantemizmi) ekranlabel1.Text = "";
+            _ekrantemizmi = false;
+            if (ekranlabel1.Text == "0") ekranlabel1.Text = "";
+            ekranlabel1.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (_ekrantemizmi) ekranlabel1.Text = "";
+            _ekrantemizmi = false;
+            if (ekranlabel1.Text == "0") ekranlabel1.Text = "";
+            ekranlabel1.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (_ekrantemizmi) ekranlabel1.Text = "";
+            _ekrantemizmi = false;
+            if (ekranlabel1.Text == "0") ekranlabel1.Text = "";
+            ekranlabel1.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (_ekrantemizmi) ekranlabel1.Text = "";
+            _ekrantemizmi = false;
+            if (ekranlabel1.Text == "0") ekranlabel1.Text = "";
+            ekranlabel1.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (_ekrantemizmi) ekranlabel1.Text = "";
+            _ekrantemizmi = false;
+            if (ekranlabel1.Text == "0") ekranlabel1.Text = "";
+            ekranlabel1.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (_ekrantemizmi) ekranlabel1.Text = "";
+            _ekrantemizmi = false;
+            if (ekranlabel1.Text == "0") ekranlabel1.Text = "";
+            ekranlabel1.Text += "9";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            _ekrantemizmi = false;
+            if (_ekrantemizmi) ekranlabel1.Text = "";
+            if (ekranlabel1.Text == "0") ekranlabel1.Text = "";
+            ekranlabel1.Text += "0";
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            _islem = '+';
+            _ekrantemizmi = true;
+            _ilksayi = Convert.ToInt32(ekranlabel1.Text);//text str olduğu için int yaptık
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            //eşittir butonu
+            int ikincisayi = Convert.ToInt32(ekranlabel1.Text);
+            int sonuc =0;
+            switch (_islem)
+            {
+                case '+':
+                    sonuc = _ilksayi + ikincisayi; break;
+                case '-':
+                    sonuc = _ilksayi - ikincisayi; break;
+                case '*':
+                    sonuc = _ilksayi * ikincisayi; break;
+                case '/':
+                    sonuc = _ilksayi / ikincisayi; break;
+               
+
+            }
+            ekranlabel1.Text = Convert.ToString(sonuc);
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            _islem = '-';
+            _ekrantemizmi = true;
+            _ilksayi = Convert.ToInt32(ekranlabel1.Text);
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            _islem = 'x';
+            _ekrantemizmi = true;
+            _ilksayi = Convert.ToInt32(ekranlabel1.Text);
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            _islem = '/';
+            _ekrantemizmi = true;
+            _ilksayi = Convert.ToInt32(ekranlabel1.Text);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            ekranlabel1.Text = "0";
+        }
+    }
+}
